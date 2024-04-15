0dda8eff96f9a573106db9779da97743c8d2e704
NonaMkrtchyan <nonamkrtcan2@gmail.com>
2024-04-15 18:32:09 +0400

    creating_and_adding_notes

---
+++ index2.js
@@ -0,0 +1,24 @@
+const btn = document.getElementById('add_btn');
+const noteContainer = document.getElementById('note_container');
+
+function createNoteElement(id, content) {
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  element.placeholder = "Empty Note";
+  element.value = content;
+  return element;
+}
+
+function addNote() {
+
+    const noteObj = {
+        id: Math.floor(Math.random() * 10000),
+        content: ""
+    }
+
+    const noteElement = createNoteElement(noteObj.id, noteObj.content);
+    noteContainer.insertBefore(noteElement, btn);
+    
+}
+
+btn.addEventListener('click', addNote)
